b8309ee7cab8e4173c0de3dfec34881f440fd9bd
NorthWestTrees <47284617+northwesttreesgaming@users.noreply.github.com>
2026-07-08 06:07:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const productCost = document.getElementById("productcost");
 
 document.querySelectorAll('input[name="products"]').forEach(radio => {
     radio.addEventListener("change", function () {
-        productCost.textContent = "$" + this.dataset.cost;
+        productCost.textContent = "$" + this.dataset.productcost;
     });
 });
